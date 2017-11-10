5c30ad4781819be74f3b53eb3a7e1f5aab9befbd
Tollef Jørgensen <tollefj@gmail.com>
2017-11-10 11:29:16 +0100

    Site revision (#9)
    * revised about pages
    
    * card sitewide style
    
    * notfound page
    
    * favicon
    
    * new register page
    
    * redesigned about page

--- client/app/about/about.component.ts
+++ client/app/about/about.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  templateUrl: './about.component.html'
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
-
+  title = 'DiscoverHS';
   constructor() { }
 
 }
